0c4263691613b9ffbec6f343d92a9ebbb580a796
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-28 01:37:35 +0500

    header

--- olx/src/Header.jsx
+++ olx/src/Header.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import SearchIcon from '@material-ui/icons/Search';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import AddIcon from '@material-ui/icons/Add';
+import {Link} from "react-router-dom";
 
 const Header=()=>{
     return(
-        <div className="header fixed">
+        <div className="header fixed flex aic">
             <div className="logo">
             <img src={"logo.svg"}></img>
             </div>
-            <div className="location rel">
-                <SearchIcon/>
-                <input className="label" placeholder="Your Location" value="Pakistan" />
-                <button className="icon-search"><KeyboardArrowDownIcon/></button> 
+            <div className="location rel flex aic">
+                <SearchIcon className="icon-search ico s24" />
+                <input className="label s15 font" placeholder="Your Location" value="Pakistan" />
+                <button className="arrow"><KeyboardArrowDownIcon/></button> 
             </div>
             <div className="search flex aic">
-                <input type="text" placeholder="Find Cars, Mobile Phonrs and more..." className="query" />
-                <button className="icon-search"><SearchIcon/></button>
+                <input type="text" placeholder="Find Cars, Mobile Phonrs and more..." className="query font s15" />
+                <button className="icon-search go"><SearchIcon/></button>
             </div>
             <div className="actions flex aic">
-                <button>
+                <Link href="/account/signin" className="fontb s15">Sign in</Link>
+                <button className="sell">
                     <AddIcon/>
+                    <h2 className="s15 font">Sell</h2>
                 </button>
             </div>
         </div>

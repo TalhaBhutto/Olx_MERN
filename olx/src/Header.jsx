fe11d15b72b1363b231630976483a8ebbec15287
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-28 00:36:47 +0500

    making the header
    Added icons from material UI and made basic structure

--- olx/src/Header.jsx
+++ olx/src/Header.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import SearchIcon from '@material-ui/icons/Search';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import AddIcon from '@material-ui/icons/Add';
 
 const Header=()=>{
     return(
         <div className="header fixed">
-            <img src={"logo.png"}></img>
+            <div className="logo">
+            <img src={"logo.svg"}></img>
+            </div>
+            <div className="location rel">
+                <SearchIcon/>
+                <input className="label" placeholder="Your Location" value="Pakistan" />
+                <button className="icon-search"><KeyboardArrowDownIcon/></button> 
+            </div>
+            <div className="search flex aic">
+                <input type="text" placeholder="Find Cars, Mobile Phonrs and more..." className="query" />
+                <button className="icon-search"><SearchIcon/></button>
+            </div>
+            <div className="actions flex aic">
+                <button>
+                    <AddIcon/>
+                </button>
+            </div>
         </div>
     );
 }
